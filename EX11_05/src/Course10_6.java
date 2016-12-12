388eb8e5627ede98784ce3045ae03743e9c71259
Kevin Rogers <Kevin Rogers>
2016-12-11 22:44:31 -0700

    Initial commit

---
+++ EX11_05/src/Course10_6.java
@@ -0,0 +1,39 @@
+class Course10_6 {
+
+	public Course10_6() {
+		System.out.println("I feel like something else should be added...");
+	}
+	
+}
+/*
+public class Course10_6 {
+
+	private String courseName;
+	private String[] students = new String[100];
+	private int numberOfStudents;
+	
+	public Course10_6 (String courseName) {
+		this.courseName = courseName;
+		
+	}
+	public void addStudent (String student) {
+		students[numberOfStudents] = student;
+		numberOfStudents++;
+		
+	}
+	public String [] getStudents(){
+		return students;
+	}
+	public int getNumberOfStudents (){
+		return numberOfStudents;	
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void dropStudent(String student) {
+		
+	}
+
+
+}
+*/
---
+++ EX11_08/src/ToolKit/Transaction.java
@@ -0,0 +1,47 @@
+package ToolKit;
+
+import java.util.Date;
+
+public class Transaction {
+
+    private Date mDate; 
+    private char mType; 
+
+    private double mAmount;
+    private double mBalance; 
+    private String mDescription; 
+
+    public Transaction(char type, double amount, double balance, String description) {
+        mType = type;
+        mAmount = amount;
+        mBalance = balance;
+        mDescription = description;
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public char getType() {
+        return mType;
+    }
+
+    public double getAmount() {
+        return mAmount;
+    }
+
+    public double getBalance() {
+        return mBalance;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Type: " + mType + " Date: " + mDate + "Description: " + mDescription +
+                "Amount: " + mAmount + " Balance: " + mBalance + "\n";
+    }
+}
